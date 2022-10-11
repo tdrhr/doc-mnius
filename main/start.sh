07b2e5c22eed2e010b71f899998a54c69b26ea61
tdrhr <108738419+tdrhr@users.noreply.github.com>
2022-10-11 06:30:35 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-3799d509b5854d53b7ad1393fe8c053e}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
